9c5ad88e86f8de8eed36074f27a3414d4857d5ca
zhanfeng.zhang <zhanfeng.zhang@xqchuxing.com>
2018-08-03 09:21:29 +0800

    create xcode project

---
+++ learn_c/learn_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learn_c
+//
+//  Created by feng on 2018/8/3.
+//  Copyright © 2018 zhanfeng.zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
